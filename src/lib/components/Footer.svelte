24fe8ee29e6cdcdc20061db89cbf96f49f4ec7b3
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2025-09-06 23:08:37 +0800

    feat(footer): update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,16 +1,16 @@
-<footer class="bg-[#F76800] text-white">
-  <div class="max-w-7xl mx-auto px-6 py-8 flex flex-col md:flex-row md:items-center md:justify-between gap-6">
+<footer class="bg-[#0060a9] text-white">
+  <div class="mt-4 max-w-7xl mx-auto px-6 py-8 flex flex-col md:flex-row md:items-center md:justify-between gap-6">
     
     <!-- Motto -->
     <p class="text-lg italic font-semibold text-center md:text-left">
-      Perfect food shouldn't go to waste just because it's not pretty.
+      Still fresh. Still super. Not wasted.
     </p>
 
 		<!-- Contact details -->
     <div class="text-center md:text-right space-y-1">
-      <p class="font-semibold">Get ugly with us:</p>
+      <p class="font-semibold">Get in touch with us:</p>
       <p class="text-sm">
-        uglyph.business@gmail.com | Tim (+63) 995-134-335
+        supersale.phbusiness@gmail.com | Tim (+63) 995-134-335
       </p>
 
 		<!-- TODO: ADD SOCIAL MEDIA ICONS/LINKS -->
