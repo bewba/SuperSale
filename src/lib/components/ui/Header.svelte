d04838d70fef57f8c34abed0519d81295ac99f39
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2025-09-06 22:53:24 +0800

    feat(header): update header content

--- src/lib/components/ui/Header.svelte
+++ src/lib/components/ui/Header.svelte
@@ -13,19 +13,20 @@
 </script>
 
 <header
-  class="sticky top-0 z-50 bg-white shadow-md border-b border-gray-100 transition-transform duration-300"
+  class="sticky top-0 z-50 bg-[#eee] shadow-md border-b border-gray-100 transition-transform duration-300"
   class:hidden={!scrolled}
 >
   <div class="max-w-7xl mx-auto flex items-center justify-between p-4">
     <!-- Logo  -->
     <a href="/" class="flex items-center space-x-2">
-      <img src="/logo.png" alt="logo-ugly.ph" class="h-10 w-auto" />
+      <img src="/logo.svg" alt="SuperSale-logo" class="h-14 w-auto" />
     </a>
 
     <!-- Seller button -->
     <button
-      class="cursor-pointer bg-[#F76800] text-white px-4 py-2 rounded-lg font-semibold hover:bg-[#e65e00] transition"
-      on:click={() => goto("/protected/seller")}
+      class="cursor-pointer bg-[#0060a9] text-white px-4 py-2 rounded-lg font-semibold
+          hover:bg-[#004b82] transition-colors duration-300 ease-in-out"
+            on:click={() => goto("/protected/seller")}
     >
       Want to sell your surplus?
     </button>
