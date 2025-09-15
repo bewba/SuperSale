50cb3024c6f1868814fc1025fd3fcd47427f254c
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2025-09-15 23:56:49 +0800

    feat: add initial brand-card

--- src/routes/brand/[slug]/+page.svelte
+++ src/routes/brand/[slug]/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-	export let data: { seller: any, error?: string };
+  import Header from "$lib/components/ui/Header.svelte";
+  import BrandInfo from "$lib/components/brand/BrandInfo.svelte";
+  
+  export let data: { seller: any, error?: string };
 </script>
 
-{#if data.seller}
-  <h1>{data.seller.store_name}</h1>
-{:else}
-  <p>{data.error ?? 'Seller not found'}</p>
-{/if}
+<div class="min-h-[100vh]">
+  <Header />
+
+  <div class="mt-8 px-0 sm:px-6 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
+    
+    <div class="px-4 sm:px-0">
+      <BrandInfo brand={data.seller} />
+
+    </div> 
+  </div>
+</div>
