f8b4eaccaf7838acc169fa84008f3b3f58b0ff09
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2025-09-15 23:39:52 +0800

    feat(brand-page): created brand slug

---
+++ src/routes/brand/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let data: { seller: any, error?: string };
+</script>
+
+{#if data.seller}
+  <h1>{data.seller.store_name}</h1>
+{:else}
+  <p>{data.error ?? 'Seller not found'}</p>
+{/if}
