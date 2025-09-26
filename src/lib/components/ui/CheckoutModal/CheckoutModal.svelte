8ea1c5c877986c70e6c8efd167fb86a33384998c
bewba <lumaguihans0123@gmail.com>
2025-09-26 22:04:41 +0800

    feat: added analytics lib and a imrpoved on the checkoutmodal.svelte

--- src/lib/components/ui/CheckoutModal/CheckoutModal.svelte
+++ src/lib/components/ui/CheckoutModal/CheckoutModal.svelte
@@ -2,7 +2,7 @@
 	import { createEventDispatcher } from 'svelte';
 	import ImageRoll from './ImageRoll.svelte';
 	import type { Deal } from '$lib/types/types';
-	import * as amplitude from '@amplitude/analytics-browser';
+	import { track } from '$lib/analytics/analytics';
 
 	export let selectedDeal: Deal;
 
@@ -58,12 +58,11 @@
 	}
 
 	function openViber() {
-		if (window?.amplitude) {
-			window.amplitude.track('Open Viber', {
-				seller_id: sellerData.id,
-				viber_link: sellerData.viber_link
-			});
-		}
+		track('Open Viber', {
+			seller_id: sellerData.id,
+			viber_link: sellerData.viber_link
+		});
+
 		setTimeout(() => {
 			window.location.href = sellerData.viber_link;
 		}, 200);
